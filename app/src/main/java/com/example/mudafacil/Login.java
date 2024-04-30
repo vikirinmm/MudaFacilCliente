d64a8fe70ad83814e69e80e59997552588598f64
vinicius <vinicius.mlemes@goias.gov.br>
2024-04-30 15:38:54 -0300

    1.0.2

--- app/src/main/java/com/example/mudafacil/Login.java
+++ app/src/main/java/com/example/mudafacil/Login.java
@@ -3,6 +3,7 @@ package com.example.mudafacil;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -28,6 +29,14 @@ public class Login extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent main = new Intent(Login.this, Cadastro_Cliente.class);
+                startActivity(main);
+            }
+        });
+        Button logar = (Button) findViewById(R.id.logar);
+        logar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent main = new Intent(Login.this, TelaPrincipal.class);
                 startActivity(main);
             }
         });
