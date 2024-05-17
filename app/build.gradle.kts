bb69575447c467d7fa017c04311b8847f9c9a7d5
vinicius <vinicius.mlemes@goias.gov.br>
2024-05-17 12:07:33 -0300

    dependencias do mapa

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     alias(libs.plugins.androidApplication)
-
 }
 
 android {
@@ -37,13 +36,17 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
-    implementation(libs.play.services.maps)
     implementation(libs.navigation.fragment)
     implementation(libs.navigation.ui)
     implementation(libs.annotation)
     implementation(libs.lifecycle.livedata.ktx)
     implementation(libs.lifecycle.viewmodel.ktx)
     implementation(libs.play.services.location)
+
+    // Adicionando novas dependências para Google Maps
+    implementation(libs.play.services.maps)
+    implementation(libs.google.maps.services)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
