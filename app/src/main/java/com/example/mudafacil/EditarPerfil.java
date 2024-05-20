c0d1272b32b827728b81eb3fe5222642e77bfc52
vinicius <vinicius.mlemes@goias.gov.br>
2024-05-20 16:03:55 -0300

    ajustes de telas

--- app/src/main/java/com/example/mudafacil/EditarPerfil.java
+++ app/src/main/java/com/example/mudafacil/EditarPerfil.java
@@ -3,11 +3,13 @@ package com.example.mudafacil;
 import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -15,11 +17,55 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class EditarPerfil extends AppCompatActivity {
+    private EditText senha;
+    private ImageView visivil1;
+    private EditText comfirmarsenha;
+    private ImageView visivil2;
+    private boolean pontos1 = false;
+    private boolean pontos2 = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.editar_perfil);
+
+
+        senha = findViewById(R.id.senha);
+        visivil1 = findViewById(R.id.olho1);
+
+        visivil1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (pontos1) {
+                    senha.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                    visivil1.setImageResource(R.drawable.visualizar_senha);
+                } else {
+                    senha.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                    visivil1.setImageResource(R.drawable.line);
+                }
+                pontos1 = !pontos1;
+                senha.setSelection(senha.getText().length());
+            }
+        });
+
+        comfirmarsenha = findViewById(R.id.comfirmarsenha);
+        visivil2 = findViewById(R.id.olho2);
+
+        visivil2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (pontos2) {
+                    comfirmarsenha.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                    visivil2.setImageResource(R.drawable.visualizar_senha);
+                } else {
+                    comfirmarsenha.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                    visivil2.setImageResource(R.drawable.line);
+                }
+                pontos2 = !pontos2;
+                comfirmarsenha.setSelection(comfirmarsenha.getText().length());
+            }
+        });
+
 
         ImageView voltar = (ImageView) findViewById(R.id.voltar);
 
--- app/src/main/java/com/example/mudafacil/Login.java
+++ app/src/main/java/com/example/mudafacil/Login.java
@@ -2,19 +2,50 @@ package com.example.mudafacil;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Login extends AppCompatActivity {
+    private EditText senhaEditText;
+    private ImageView toggleSenhaImageView;
+    private boolean isPasswordVisible = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.login);
+
+
+
+        senhaEditText = findViewById(R.id.senha);
+        toggleSenhaImageView = findViewById(R.id.olho);
+
+        toggleSenhaImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isPasswordVisible) {
+                    senhaEditText.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                    toggleSenhaImageView.setImageResource(R.drawable.visualizar_senha);
+                } else {
+                    senhaEditText.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                    toggleSenhaImageView.setImageResource(R.drawable.line);
+                }
+                isPasswordVisible = !isPasswordVisible;
+                senhaEditText.setSelection(senhaEditText.getText().length());
+            }
+        });
+
+
+
+
 
         TextView recuperar = (TextView) findViewById(R.id.recuperarsenha);
         recuperar.setOnClickListener(new View.OnClickListener() {
